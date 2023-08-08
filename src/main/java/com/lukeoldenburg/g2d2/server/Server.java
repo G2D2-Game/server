2f4f18e6948d52ba677625715122ceecf97ff55a
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2023-08-08 17:20:33 -0400

    Add level gen code.

--- src/main/java/com/lukeoldenburg/g2d2/server/Server.java
+++ src/main/java/com/lukeoldenburg/g2d2/server/Server.java
@@ -1,15 +1,22 @@
 package com.lukeoldenburg.g2d2.server;
 
 import com.google.gson.JsonObject;
+import com.lukeoldenburg.g2d2.server.level.Level;
 import org.java_websocket.server.WebSocketServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.net.InetSocketAddress;
 import java.nio.file.Path;
 import java.util.Objects;
 
 public class Server {
+	private static final Logger logger = LoggerFactory.getLogger(Server.class);
+	private static JsonObject config;
+	private static WebSocketServer webSocketServer;
+	private static Level level;
+
 	public static JsonObject getConfig() {
 		return config;
 	}
@@ -18,14 +25,16 @@ public class Server {
 		return logger;
 	}
 
-	private static final Logger logger = LoggerFactory.getLogger(Server.class);
-	private static JsonObject config;
-	private static WebSocketServer webSocketServer;
-
 	public static void main(String[] args) {
 		logger.info("Loading...");
 		registerHooks();
+
 		config = Objects.requireNonNullElse(JsonFile.read(Path.of("config.json"), JsonObject.class), new JsonObject());
+
+		File levelFile = new File(Objects.requireNonNullElse(config.get("levelPath").getAsString(), "level.bin.gz"));
+		if (levelFile.exists() && !levelFile.isDirectory()) level = new Level("level.bin.gz");
+		else level = new Level();
+
 		webSocketServer = new ServerSocket(new InetSocketAddress(Objects.requireNonNullElse(config.get("port").getAsInt(), 8000)));
 	}
 
@@ -35,6 +44,7 @@ public class Server {
 			logger.info("Saving...");
 			if (JsonFile.write(Path.of("config.json"), config)) logger.info("Successfully saved config.json");
 			else logger.error("Failed to save config.json");
+			level.saveLevel();
 		}));
 	}
 }
--- src/main/java/com/lukeoldenburg/g2d2/server/level/Level.java
+++ src/main/java/com/lukeoldenburg/g2d2/server/level/Level.java
@@ -1,55 +1,151 @@
 package com.lukeoldenburg.g2d2.server.level;
 
+import com.lukeoldenburg.g2d2.server.OpenSimplexNoise;
+
+import java.io.*;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class Level {
-	private HashMap<Coordinate, Chunk> loadedChunks;
+	private static final int SIZE = 5000;
+	private String levelPath;
+	private short[][] bgTiles = new short[SIZE][SIZE];
+	private short[][] fgTiles = new short[SIZE][SIZE];
+	private Map<Coordinate, byte[]> tileData = new HashMap<>();
+
+	public Level(String levelPath) {
+		this.levelPath = levelPath;
+		try {
+			loadLevel(new BufferedInputStream(new FileInputStream(levelPath)));
+
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	public Level() {
-		this.loadedChunks = new HashMap<>();
+		this.levelPath = "level.bin.gz";
+		generateLevel();
 	}
 
-	public Chunk getChunk(Coordinate chunkLocation) {
-		return loadedChunks.get(chunkLocation);
+	public Level(byte[] data) {
+		loadLevel(new BufferedInputStream(new ByteArrayInputStream(data)));
 	}
 
-	public void setChunk(Coordinate chunkLocation, Chunk chunk) {
-		loadedChunks.put(chunkLocation, chunk);
-	}
+	private Map<Coordinate, byte[]> loadTileData(GZIPInputStream gzis) throws IOException {
+		byte[] buffer = new byte[4];
+		int bytesRead;
 
-	public short getTile(int tileX, int tileY, boolean isForeground) {
-		Chunk chunk = getChunk(CoordinateConverter.tileToChunkCoords(tileX, tileY));
-		if (chunk == null) {
-			// Chunk not loaded, handle as needed (load from disk or create new chunk).
-			return 0;
+		while ((bytesRead = gzis.read(buffer)) != -1) {
+			if (bytesRead < 4) {
+				// Insufficient bytes to read Coordinate data, break the loop
+				break;
+			}
+
+			int maxX = (buffer[0] << 8) | (buffer[1] & 0xFF);
+			int maxY = (buffer[2] << 8) | (buffer[3] & 0xFF);
+
+			ByteArrayOutputStream dataStream = new ByteArrayOutputStream();
+			while (true) {
+				int dataByte = gzis.read();
+				if (dataByte == -1) {
+					break; // No more data to read
+				}
+
+				dataStream.write(dataByte);
+			}
+
+			tileData.put(new Coordinate(maxX, maxY), dataStream.toByteArray());
 		}
 
-		int tileXInChunk = tileX % 256;
-		int tileYInChunk = tileY % 256;
-		if (isForeground) {
-			return chunk.getFgTile(tileXInChunk, tileYInChunk);
+		return tileData;
+	}
 
-		} else {
-			return chunk.getBgTile(tileXInChunk, tileYInChunk);
+	private void loadLevel(BufferedInputStream bis) {
+		try {
+			GZIPInputStream gzis = new GZIPInputStream(bis);
+
+			byte[] buffer = new byte[50000000];
+			int bytesRead = gzis.read(buffer);
+
+			short[][] bgTiles = new short[SIZE][SIZE];
+			for (int x = 0; x < SIZE; x++) {
+				for (int y = 0; y < SIZE; y++) {
+					int index = x * SIZE + y;
+					bgTiles[x][y] = (short) ((buffer[index * 2] << 8) | (buffer[index * 2 + 1] & 0xFF));
+				}
+			}
+
+			bytesRead = gzis.read(buffer);
+
+			short[][] fgTiles = new short[SIZE][SIZE];
+			for (int x = 0; x < SIZE; x++) {
+				for (int y = 0; y < SIZE; y++) {
+					int index = x * SIZE + y;
+					fgTiles[x][y] = (short) ((buffer[index * 2] << 8) | (buffer[index * 2 + 1] & 0xFF));
+				}
+			}
+
+			loadTileData(gzis);
+			gzis.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
-	public void setTile(int tileX, int tileY, short tile, boolean isForeground) {
-		Coordinate chunkCoords = CoordinateConverter.tileToChunkCoords(tileX, tileY);
+	public void saveLevel() {
+		try {
+			FileOutputStream fos = new FileOutputStream(levelPath);
+			BufferedOutputStream bos = new BufferedOutputStream(fos);
+			GZIPOutputStream gzos = new GZIPOutputStream(bos);
 
-		Chunk chunk = getChunk(chunkCoords);
-		if (chunk == null) {
-			// Chunk not loaded, handle as needed (load from disk or create new chunk).
-			chunk = new Chunk(chunkCoords);
-			setChunk(chunkCoords, chunk);
+			// Write bgTiles
+			for (int x = 0; x < SIZE; x++) {
+				for (int y = 0; y < SIZE; y++) {
+					short value = bgTiles[x][y];
+					gzos.write((value >> 8) & 0xFF);
+					gzos.write(value & 0xFF);
+				}
+			}
+
+			// Write fgTiles
+			for (int x = 0; x < SIZE; x++) {
+				for (int y = 0; y < SIZE; y++) {
+					short value = fgTiles[x][y];
+					gzos.write((value >> 8) & 0xFF);
+					gzos.write(value & 0xFF);
+				}
+			}
+
+			// Write map data
+			for (Map.Entry<Coordinate, byte[]> entry : tileData.entrySet()) {
+				Coordinate coordinate = entry.getKey();
+				byte[] data = entry.getValue();
+
+				gzos.write((coordinate.getX() >> 8) & 0xFF);
+				gzos.write(coordinate.getX() & 0xFF);
+				gzos.write((coordinate.getY() >> 8) & 0xFF);
+				gzos.write(coordinate.getY() & 0xFF);
+				gzos.write(data);
+			}
+
+			gzos.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+	}
 
-		int tileXInChunk = tileX % 256;
-		int tileYInChunk = tileY % 256;
-		if (isForeground) {
-			chunk.setFgTile(tileXInChunk, tileYInChunk, tile);
-		} else {
-			chunk.setBgTile(tileXInChunk, tileYInChunk, tile);
+	public void generateLevel() {
+		double FREQUENCY = 1 / 24.0;
+		for (int x = 0; x < SIZE; x++) {
+			for (int y = 0; y < SIZE; y++) {
+				short value = (short) Math.floor(OpenSimplexNoise.noise3_ImproveXY(System.nanoTime(), x * FREQUENCY, y * FREQUENCY, 0.0) * 100);
+				bgTiles[x][y] = value;
+			}
 		}
 	}
 }
