1368045f00598a513316121a6f987658944b348e
Luke Oldenburg <87272260+Luke-Oldenburg@users.noreply.github.com>
2024-03-08 20:22:19 -0500

    Add version number

--- src/main/java/com/lukeoldenburg/g2d2/server/Server.java
+++ src/main/java/com/lukeoldenburg/g2d2/server/Server.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class Server {
+	public static final String VERSION = "1.0.0-alpha.1";
 	private static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
 	private static JsonObject config;
 	private static WebSocketServer webSocketServer;
